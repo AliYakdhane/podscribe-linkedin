c6e4afcf3a399b31fd65ea332a6d572ecf6d3654
Ali Yakdhane <yakdhanali97@gmail.com>
2026-03-13 14:52:11 +0100

    new podcasts

--- backend/main.py
+++ backend/main.py
@@ -37,6 +37,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:4200",
         "http://127.0.0.1:4200",
+        "https://podcast-ai-studio-jjlp.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
